b883ab8ca372df634e1383560d05263393680879
M.Amin <m.a.abbasfar@gmail.com>
2023-06-06 16:17:31 +0330

    touch input added

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,13 +4,45 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    public float speed = 0.05f;
+    public Transform cameraTransform;
+    public Transform sampleGroundTransform;
+
+    private float leftLimit;
+    private float rightLimit;
+
     void Start()
     {
+        leftLimit = -sampleGroundTransform.localScale.x/2f + 1f;
+        rightLimit = sampleGroundTransform.localScale.x/2f - 1f;
+
     }
 
     void Update()
     {
         checkKeyPress();
+        checkTouchInput();
+
+        transform.Translate(new Vector3(0, 0, speed));
+        cameraTransform.position += new Vector3(0, 0, speed);
+    }
+
+    private void checkTouchInput()
+    {
+        if(Input.touchCount>0)
+        {
+            Touch touch = Input.touches[0];
+            if(touch.phase == TouchPhase.Moved)
+            {
+                Vector3 characterNewPosition = new Vector3(touch.deltaPosition.x / 100.0f, 0, 0) + transform.position;
+                if (characterNewPosition.x < leftLimit)
+                    transform.position = new Vector3(leftLimit, transform.position.y, transform.position.z);
+                else if (characterNewPosition.x > rightLimit)
+                    transform.position = new Vector3(rightLimit, transform.position.y, transform.position.z);
+                else
+                    transform.position = characterNewPosition;
+            }
+        }
     }
 
     private void checkKeyPress()
---
+++ Assets/Scripts/GroundGenerator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundGenerator : MonoBehaviour
+{
+    public GameObject sampleGround;
+    private Transform characterTransform;
+
+    private Queue<GameObject> grounds = new Queue<GameObject>();
+
+    private const float initialCharacterPosition = -1000f;
+    private const int numberOfGroundsInGame = 5;
+    void Start()
+    {
+        characterTransform = transform;
+        for(int i=0;i<numberOfGroundsInGame;i++)
+        {
+            generateNewGround(initialCharacterPosition + i * sampleGround.transform.localScale.z);
+        }
+    }
+
+    void Update()
+    {
+        GameObject firstGround = grounds.Peek();
+
+        if(characterTransform.position.z > firstGround.transform.position.z + sampleGround.transform.localScale.z)
+        {
+            grounds.Dequeue().SetActive(false);
+            generateNewGround(grounds.Peek().transform.position.z + grounds.Count*sampleGround.transform.localScale.z);
+        }
+    }
+
+    private void generateNewGround(float zPosition)
+    {
+        GameObject ground = GameObject.Instantiate(sampleGround);
+        ground.SetActive(true);
+        ground.transform.position = new Vector3(0, 0, zPosition);
+        grounds.Enqueue(ground);
+    }
+}
